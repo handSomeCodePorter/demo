2cea0abc32c72005a9a080ca539e952c78a62e75
SW9520 <hi_sunwen@163.com>
2019-11-21 18:01:10 +0800

    添加爬虫模块

---
+++ zhpt-service/src/main/java/com/cloud/zhpt/log/LogAcpect.java
@@ -0,0 +1,50 @@
+package com.cloud.zhpt.log;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+
+/**
+ * @ClassName LogAcpect
+ * @Description
+ * @Author SW
+ * @Date 2019/11/15 10:04
+ **/
+@Aspect
+@Component
+public class LogAcpect {
+    private Logger logger = LoggerFactory.getLogger(LogAcpect.class);
+
+
+    /**
+     * 定义切入点，切入点为com.example.aop下的所有函数
+     */
+    @Pointcut("execution(public * com.cloud.zhpt.*.controller..*.*(..))")
+    public void webLog(){}
+
+
+    @Before("webLog()")
+    public void doBefore(JoinPoint joinPoint) throws Throwable {
+        // 接收到请求，记录请求内容
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+
+        // 记录下请求内容
+        logger.info("URL : " + request.getRequestURL().toString());
+        logger.info("HTTP_METHOD : " + request.getMethod());
+        logger.info("IP : " + request.getRemoteAddr());
+        logger.info("CLASS_METHOD : " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
+        logger.info("ARGS : " + Arrays.toString(joinPoint.getArgs()));
+    }
+
+}
